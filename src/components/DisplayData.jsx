181b239fc1d607a3739a14f7eef09f0e87db4d6c
PreetiNegi2709 <105388761+PreetiNegi2709@users.noreply.github.com>
2025-10-30 18:41:17 +0530

    Fix(refactor): Extra Comment and fragment removed.

--- src/components/DisplayData.jsx
+++ src/components/DisplayData.jsx
@@ -1,42 +1,36 @@
 import { formatter } from "../util/investment";
 const DisplayData = ({ anuualData }) => {
   return (
-    <>
-      {/* {anuualData.length < 1 ? (
-        <p className="center">Invalid Duration</p>
-      ) : ( */}
-      <table id="result">
-        <thead>
-          <tr>
-            <th>Years</th>
-            <th>Investment Value</th>
-            <th>Interest (Year)</th>
-            <th>Total Interest</th>
-            <th>Invested Capital</th>
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Years</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {anuualData.length < 1 ? (
+          <tr className="center">
+            <td colSpan="5">Incorrect Duration</td>
           </tr>
-        </thead>
-        <tbody>
-          {anuualData.length < 1 ? (
-            <tr className="center">
-              <td colSpan="5">Incorrect Duration</td>
-            </tr>
-          ) : (
-            anuualData.map((singleYear) => {
-              return (
-                <tr key={singleYear.year}>
-                  <td>{singleYear.year}</td>
-                  <td>{formatter.format(singleYear.valueEndOfYear)}</td>
-                  <td>{formatter.format(singleYear.interest)}</td>
-                  <td>{formatter.format(singleYear.interest)}</td>
-                  <td>{formatter.format(singleYear.annualInvestment)}</td>
-                </tr>
-              );
-            })
-          )}
-        </tbody>
-      </table>
-      {/* )} */}
-    </>
+        ) : (
+          anuualData.map((singleYear) => {
+            return (
+              <tr key={singleYear.year}>
+                <td>{singleYear.year}</td>
+                <td>{formatter.format(singleYear.valueEndOfYear)}</td>
+                <td>{formatter.format(singleYear.interest)}</td>
+                <td>{formatter.format(singleYear.interest)}</td>
+                <td>{formatter.format(singleYear.annualInvestment)}</td>
+              </tr>
+            );
+          })
+        )}
+      </tbody>
+    </table>
   );
 };
 
