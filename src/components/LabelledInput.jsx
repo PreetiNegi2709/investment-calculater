9a14ee91789f2358554f8e3429e5a6c247d7c7c2
PreetiNegi2709 <105388761+PreetiNegi2709@users.noreply.github.com>
2025-11-01 11:55:10 +0530

    fix: tag issue resolved.

--- src/components/LabelledInput.jsx
+++ src/components/LabelledInput.jsx
@@ -1,9 +1,9 @@
 const LabelledInput = ({ value, onChange, label }) => {
   return (
-    <div>
+    <>
       <label htmlFor={label}>{label}</label>
       <input id={label} type="number" value={value} onChange={onChange} />
-    </div>
+    </>
   );
 };
 
